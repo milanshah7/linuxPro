2662e69fd64e58cc7bb11d5fe37f482f1e84821d
Milan Shah <milanshah7@ymail.com>
2019-10-12 22:35:48 +0530

    added wrappers for message queues and make changes accodingly in makefile

---
+++ msgQ.c
@@ -0,0 +1,28 @@
+/*************** Library Inclusion **************/
+
+#include "msgQ.h"
+
+/*************** Function Defination ***************/
+
+mqd_t msgQ_open(const char *mqName, uint32_t mqSize){
+	
+	struct mq_attr attr;
+	
+	int mqFlag      = O_RDWR | O_CREAT;
+	mode_t mqMode   = S_IRWXU;
+	attr.mq_msgsize = mqSize;
+
+	return mq_open(mqName, mqFlag, mqMode, &attr);
+}
+
+int msgQ_send(mqd_t mqd, const char *mPtr, size_t mPtr_size){
+	return mq_close(mqd, mPtr, mPtr_size, 0);
+}
+
+ssize_t msgQ_receive(mqd_t mqd, char *mPtr, size_t mPtr_size){
+	return mq_receive(mqd, mPtr, mPtr_size, NULL);
+}
+
+int msgQ_unlink(const char *mqName){
+	return mq_unlink(mqName);
+}
---
+++ msgQ.h
@@ -0,0 +1,90 @@
+/************* Library Inclusion ****************/
+
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#endif
+
+#include <mqueue.h>
+
+/************** MACRO Defination ****************/
+
+#define MSG_ID                 1
+#define MSG_ID_LINKED_LIST     (MSG_ID << 0)
+#define MSG_ID_PTHREAD_OPS     (MSG_ID << 1)
+#define MSG_ID_FORK_OPS        (MSG_ID << 2)
+#define MSG_ID_IPC_OPS         (MSG_ID << 3)
+
+#define MSGQ_NAME              "/myqueue"
+#define MSGQ_DATA_SIZE         sizeof(struct msgQ_struct)
+
+/*************** Variable Declarations ***************/
+
+struct msgQ_struct{
+	uint32_t msgid;
+	bool bstatus;
+	union payloadData msgData;
+};
+
+/************* Function Declarations *************/
+
+/*
+ * msgQ_open - to open a message queue
+ *
+ * in param   :
+ * 		mqName - message queue name
+ * 		mqSize - Data size which to be tranmissted over message queue
+ * out param  : 
+ * 		None
+ * return val :
+ * 		message queue descriptor on success, else -1 to indicate failure
+ *
+ */
+
+mqd_t msgQ_open(const char *mqName, uint32_t mqSize);
+
+/*
+ * msgQ_send - to send some data over message queue
+ *
+ * in param   : 
+ * 		mqd       - message queue descriptor
+ * 		mPtr      - pointer to the data which needs to be transmitted
+ * 		mPtr_size - size of the data
+ * out param  : 
+ * 		None
+ * return val : 
+ * 		0 on success, else -1 to indicate failure
+ *
+ */
+
+int msgQ_send(mqd_t mqd, const char *mPtr, size_t mPtr_size);
+
+/*
+ * msgQ_receive - to receive a message over message queue
+ *
+ * in param   : 
+ * 		mqd       - message queue descriptor
+ * 		mPtr      - pointer to the data which is to be received
+ * 		mPtr_size - size of the data
+ * out param  : 
+ * 		None
+ * return val :
+ * 		no. of bytes in the received message, else -1 to indicate failure
+ *
+ */
+
+ssize_t msgQ_receive(mqd_t mqd, char *mPtr, size_t mPtr_size);
+
+/*
+ * msgQ_unlink - to remove a message queue
+ *
+ * in param   :
+ * 		mqName - message queue name
+ * out param  :
+ * 		None
+ * return val :
+ * 		0 on success, else -1 to indicate failure
+ *
+ */
+
+int msgQ_unlink(const char *mqName);
